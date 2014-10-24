d1ea440cdab15d34f5f1e780cc7b7dee5ac12ffa
Tom Mitchell <tom@tom.net.nz>
2014-10-24 12:55:02 -0700

    Initial commit

---
+++ color.h
@@ -0,0 +1,26 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+class Color {
+public:
+    // Constructors
+    Color();
+    Color(uint8_t r, uint8_t g, uint8_t b);
+    Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+    // Destructor
+    ~Color();
+    
+    // Methods
+    uint32_t ToValue();
+    
+    // Static helpers
+    static Color FromValue(const uint32_t value);
+    static Color BlendAlpha(const Color colors[]);
+    
+private:
+    uint8_t r, g, b, a;
+};
+
+// Helper functions
+
+#endif // COLOR_H
