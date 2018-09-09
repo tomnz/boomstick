6c2cfd2f7e328f151d34d6df1ea4a779339d465f
Tom Mitchell <tom@tom.net.nz>
2018-09-09 18:55:55 -0400

    Update hood config

--- hood.config.h
+++ hood.config.h
@@ -1,36 +1,33 @@
 #ifndef CONFIG
 #define CONFIG
 
+#define ENABLE_SERIAL
+
 //////////////////////////
 // LED HARDWARE SETTINGS
 
 // LED strand data pin
-#define LED_PIN     6
+#define LED_PIN     5
+#define LED_COLOR_ORDER GRB
 
 // Number of pixels in strand
-#define N_PIXELS    27
-
-// When enabled, uses N_PIXELS * 2 pixels on the data line, mirrored in the
-// center
-#define MIRROR_DUPE
-
-// When enabled, reverses the ordering of the LED output
-#define FLIP
-
-// "End" of the string - one past the end so the peak dot can move off scale
-#define TOP         (N_PIXELS + 2)
+#define N_PIXELS    52
 
 // When defined, uses an analog signal to vary the overall brightness
-#define BRIGHTNESS_PIN 4
+#define BRIGHTNESS_PIN 0
 
 // Which effect to use on startup - if no button is present, then this
 // is the only effect which is used
 //   0 = Bars
 //   1 = Pulse
+//   2 = Noise
+//   3 = Sinelon
+//   4 = Fire
 #define INITIAL_EFFECT 0
 
 // When defined, enables a button on a digital input to change effects
-#define EFFECT_BUTTON_PIN 8
+#define EFFECT_BUTTON_PIN 7
+
 
 ////////////////////
 // AUDIO SETTINGS
@@ -50,40 +47,67 @@
 // Amount of smoothing for current volume value
 #define SMOOTH_FACTOR 3.0
 
+// Boost all effects
+#define BASE_LEVEL_SCALE   1.6
+
 // Historic FFT frames to keep to determine high/low range
 #define HISTORIC_FRAMES 40
 #define NOISE_THRESHOLD 5
 #define LEVEL_CUTOFF 30
 
+// Sanitized level is designed to be a less "jumpy" level source. It will always be
+// in the range [0.0, 1.0]. It operates in a peak/decay fashion - if the current level
+// is higher, it will jump to that level, but then decays at a set rate if the level
+// drops again.
+#define SANITIZED_LEVEL_SCALE 1.4
+// The incoming level signal must be greater than this minimum to have any effect
+#define SANITIZED_LEVEL_MIN 0.4
+// Instead of directly using the level, treat it like a peak, decaying it at a fixed rate
+#define SANITIZED_LEVEL_DECAY 0.05
+
+
 /////////////////////////
 // BAR EFFECT SETTINGS
 
-// Color for the minimum intensity bar
-#define MIN_COL     70
-// Color for the maximum intensity bar
-#define MAX_COL     255
-// Derived color range between min/max
-#define COL_RANGE   (MAX_COL - MIN_COL)
-// Color difference between lower and upper parts of bar
-#define COL_VAR     60
+#define BAR_MIRROR true
+// When defined, reverses the direction of the bar
+#define BAR_FLIP
 
-#define BAR_SCALE   1.4
+// Boost bar effect
+#define BAR_LEVEL_SCALE 1.0
+
+// Color for the low intensity bar
+#define BAR_COL_LOW     0
+// Color for the high intensity bar - must be greater than min
+#define BAR_COL_HIGH     160
+// When defined, inverts high/low bar color spectrum
+#define BAR_COL_INVERT
+// Derived color range between min/max
+#define BAR_COL_RANGE   (BAR_COL_HIGH - BAR_COL_LOW)
+// Color difference between lower and upper parts of bar
+#define BAR_COL_VAR     60
+
 #define MIN_BAR_SIZE 70
 
 // Rate at which peak dot falls
-#define PEAK_FALL_RATE 0.15
+#define PEAK_FALL_RATE 0.2
+
+#define PEAK_RADIUS 1
 
 // Background color settings
-#define BACKGROUND_COLOR 80
+#define BACKGROUND_COLOR 160
 #define BACKGROUND_CUTOFF (N_PIXELS / 3)
-#define BACKGROUND_MAX 0.2
-#define BACKGROUND_INCREASE 0.001
+#define BACKGROUND_MAX 0.18
+#define BACKGROUND_INCREASE 0.0005
 #define BACKGROUND_DECREASE 0.01
 
 #define HISTORIC_SCALE 1.2
 
+
 ///////////////////////////
 // PULSE EFFECT SETTINGS
+
+#define PULSE_MIRROR false
 
 // High/low values for transformed level that determine when a beat
 // stops and starts
@@ -110,5 +134,86 @@
 
 #define PULSE_FADE_FACTOR 20.0
 #define PULSE_BRIGHTNESS_BEAT 2.5
+
+
+///////////////////////////
+// NOISE EFFECT SETTINGS
+
+#define NOISE_MIRROR true
+
+#define NOISE_SCALE_BASE 70
+#define NOISE_SCALE_DELTA 100
+
+// Minimum rate at which to shift the noise pattern
+#define NOISE_SHIFT_MIN 150
+// Audio level multiplier to shift the noise pattern at
+#define NOISE_SHIFT_FACTOR 1500
+
+// The brightness and saturation are shifted for the current level, between [MIN, 255]
+#define NOISE_BRIGHTNESS_MIN 200
+#define NOISE_SATURATION_MIN 150
+
+// After applying the brightness shift for the current level, contrast is applied.
+// If the brightness is above the midpoint, it's boosted, otherwise it's reduced.
+#define NOISE_CONTRAST_MIDPOINT 50
+#define NOISE_CONTRAST_AMOUNT 0.8
+
+// If defined, makes the noise align on each end
+//#define NOISE_CIRCULAR
+
+
+/////////////////////////////
+// SINELON EFFECT SETTINGS
+
+#define SINELON_MIRROR false
+
+// The brightness and saturation are shifted for the current level, between [MIN, 255]
+#define SINELON_BRIGHTNESS_MIN 120
+#define SINELON_SATURATION_MIN 180
+
+// Number of dots in the effect
+#define SINELON_DOTS 3
+
+// Position advances by a maximum of this value, and a minimum of the MIN_LEVEL multiplied by it.
+#define SINELON_POS_RATE 150.0
+#define SINELON_POS_MIN_LEVEL 0.1
+
+// Ditto for hue - worth noting that small values will round down to 0, and the saturation won't
+// advance at all.
+#define SINELON_HUE_RATE 8.0
+#define SINELON_HUE_MIN_LEVEL 0.1
+
+// Number of oscillators per dot - increasing this adds randomness, but increases the chance of
+// the dot being in the middle most of the time
+#define SINELON_NUM_OSCILLATORS 2
+
+// Min/max period defines how quickly each of the oscillators for a dot will move
+#define SINELON_PERIOD_MIN 15
+#define SINELON_PERIOD_MAX 40
+
+
+//////////////////////////
+// FIRE EFFECT SETTINGS
+
+#define FIRE_MIRROR true
+
+// For these parameters, the min is used when the level is zero, and max when the level is at maximum.
+
+// How much does the air cool as it rises?
+// Less cooling = taller flames. More cooling = shorter flames. Tweak for the number of LEDs.
+#define FIRE_COOLING_MIN  45
+#define FIRE_COOLING_MAX  45
+
+// What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire. Lower chance = more flickery fire.
+#define FIRE_SPARKING_MIN 30
+#define FIRE_SPARKING_MAX 160
+
+// Heat for each new spark. Also used to maintain a continuous "flame" at the bottom
+#define FIRE_SPARK_HEAT_MIN 100
+#define FIRE_SPARK_HEAT_MAX 200
+
+// If the animation is too quick, we can artificially slow it with some delay
+#define FIRE_FRAME_DELAY_MS 4
 
 #endif
